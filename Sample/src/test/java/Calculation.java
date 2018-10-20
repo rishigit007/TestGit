4c6581414240e681b17aa8c79763575a36d36a61
indranil goswami <indranil.goswami14@gmail.com>
2018-10-20 20:59:55 +0530

    first code deployed in Git repository

---
+++ Sample/src/test/java/Calculation.java
@@ -0,0 +1,21 @@
+package test.java;
+
+public class Calculation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int result = add(5,7);
+		System.out.println("result is :::"+result);
+
+	}
+	
+	static int add(int x , int y)
+	{
+		return x+y;
+	}
+
+}
